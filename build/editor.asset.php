4582814c5012fca8e6070e5ab9fd4656843476ed
Nick Diego <nickmdiego@gmail.com>
2025-01-21 09:24:05 -0600

    Fresh build.

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-hooks', 'wp-i18n'), 'version' => '827a3b54967f9c40c17d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-hooks', 'wp-i18n'), 'version' => 'ded29980a58b0b14ee81');
